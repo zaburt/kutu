8bd21bbe65e89ab603e762dfbcbee602000c68ed
Onur Küçük <onur@delipenguen.net>
2015-12-14 00:49:25 +0200

    add layout partials

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,9 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
-//= require_tree .
 
-$(function(){ $(document).foundation(); });
+
+$(function(){
+  $(document).foundation();
+});
+
