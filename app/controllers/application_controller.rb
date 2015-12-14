b4a54611992a81cf7346e2c1721eceb4ebae2ef6
Onur Küçük <onur@delipenguen.net>
2015-12-14 02:00:14 +0200

    set default language to tr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,26 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery :with => :exception
 
+  before_filter :set_locale
   before_filter :set_time_zone
   before_filter :set_dynamic_hosts
+  before_filter :authenticate_as_needed!
 
 
   private
 
+  SKIP_AUTH_FOR = {
+    'home' => ['index']
+  }
+
+  REQUIRE_AUTH_FOR = [
+    'admin'
+  ]
+
+  def authenticate_as_needed!
+    return if SKIP_AUTH_FOR.include?(controller_name) && SKIP_AUTH_FOR[controller_name].include?(action_name)
+    authenticate_user! if REQUIRE_AUTH_FOR.include?(controller_name)
+  end
 
   def set_dynamic_hosts
     ActionController::Base.asset_host = Proc.new { |source, request|
@@ -25,7 +39,6 @@ class ApplicationController < ActionController::Base
   def set_time_zone
     Time.zone = 'Europe/Istanbul'
   end
-
 
 end
 
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,35 @@ module Kutu
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    ### custom settings
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Europe/Istanbul'
+
+    # Enforce I18n to check the available locales when setting a locale.
+    # The old (rails[<3.2.16]) behaviour is `false' (default is `nil' which raises a deprecation warning)
+    # See: https://github.com/svenfuchs/i18n/commit/3b6e56e06fd70f6e4507996b017238505e66608c
+    config.i18n.enforce_available_locales = true
+    I18n.config.enforce_available_locales = true # http://stackoverflow.com/a/20381730/2771989
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :tr
+
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = 'utf-8'
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
+
+    # force add fonts to asset pipeline
+    # config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
   end
 end
+
