51faa08985863c9ca3838a31204a3f83e68d3f25
Onur Küçük <onur@delipenguen.net>
2015-12-29 01:47:29 +0200

    add box shadow class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,13 @@ module ApplicationHelper
     # 'rotate_minus_24'
   ]
 
+  BOX_SHADOW_CLASS = [
+    'box_shadow_bottom_full',
+    'box_shadow_bottom_left',
+    'box_shadow_bottom_right',
+    'box_shadow_bottom_both'
+  ]
+
   def fonticon(iconname, customclass = '')
     # haml_tag :i, :class => "fi-#{iconname} #{customclass} fonticon"
     "<i class='fi-#{iconname} #{customclass} fonticon'></i>".html_safe
@@ -22,6 +29,10 @@ module ApplicationHelper
 
   def random_rotation_class
     ROTATION_CLASS.sample
+  end
+
+  def random_box_shadow_class
+    BOX_SHADOW_CLASS.sample
   end
 
 end
