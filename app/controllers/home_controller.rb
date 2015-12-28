a8b7c34c5376aea3a01b2488372e938bebb65bec
Onur Küçük <onur@delipenguen.net>
2015-12-29 00:50:44 +0200

    article display plus rotation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+    @fullwidth = true
     @articles = Article.publishable.order('publish_time DESC')
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,28 @@
 module ApplicationHelper
 
+  ROTATION_CLASS = [
+    'rotate_4',
+    'rotate_minus_4',
+    'rotate_8',
+    'rotate_minus_8',
+    'rotate_12',
+    'rotate_minus_12',
+    'rotate_16',
+    'rotate_minus_16'
+    # 'rotate_20',
+    # 'rotate_minus_20',
+    # 'rotate_24',
+    # 'rotate_minus_24'
+  ]
+
   def fonticon(iconname, customclass = '')
     # haml_tag :i, :class => "fi-#{iconname} #{customclass} fonticon"
     "<i class='fi-#{iconname} #{customclass} fonticon'></i>".html_safe
   end
 
+  def random_rotation_class
+    ROTATION_CLASS.sample
+  end
 
 end
 
