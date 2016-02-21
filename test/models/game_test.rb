9dbfe9b79fe0601f3f4b2078772e3f46643e6cae
Onur Küçük <onur@delipenguen.net>
2016-02-21 20:09:50 +0200

    update annotations

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -21,6 +21,7 @@
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 #  slug             :string
+#  active           :boolean          default(TRUE)
 #
 # Indexes
 #
