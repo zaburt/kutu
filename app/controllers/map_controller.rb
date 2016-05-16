d8ed9ec990d258fa703373b57f9a1abba4ae5f30
Onur Küçük <onur@delipenguen.net>
2016-05-17 00:06:42 +0300

    add game relations id map to map

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,6 +4,8 @@ class MapController < ApplicationController
     @fullwidth = true
     @focus_on = params[:focus_on]
     @games = Game.includes(:game_category, :city)
+    @city_hash = Hash[City.order(:name).map{|k| [k.name, k.id]}]
+    @game_category_hash = Hash[GameCategory.order(:name).map{|k| [k.name, k.id]}]
     @game_hash = {}
   end
 
