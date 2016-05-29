18b22038395dbafaceee76540f85a2bdb77e7854
Onur Küçük <onur@delipenguen.net>
2016-05-29 18:13:49 +0300

    annotate game_labels

--- app/models/game_label.rb
+++ app/models/game_label.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: game_labels
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class GameLabel < ActiveRecord::Base
 end
--- test/models/game_label_test.rb
+++ test/models/game_label_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: game_labels
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class GameLabelTest < ActiveSupport::TestCase
