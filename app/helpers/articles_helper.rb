2a98221f243abd5f1619e9b96f4b9e87cccdc29c
Onur Küçük <onur@delipenguen.net>
2016-06-07 03:25:42 +0300

    show ratings on article page

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,17 @@
 module ArticlesHelper
+
+  def rating_keys
+    [
+      :rating_puzzle,
+      :rating_fun,
+      :rating_fluency,
+      :rating_ambience,
+      :rating_management,
+      :rating_service
+    ]
+  end
+
+  def rating_name(attr_key)
+    t("activerecord.attributes.article.#{attr_key}")
+  end
 end
